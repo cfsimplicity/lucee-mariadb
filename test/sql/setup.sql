2f13f3ed8bba036832307b390cac32820424076f
cfsimplicity <github@zyde.uk>
2022-07-11 15:46:40 +0100

    Separate tests for each data type. Add int type test.

--- test/sql/setup.sql
+++ test/sql/setup.sql
@@ -1,13 +1,15 @@
 SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `datatypes`;
 CREATE TABLE `datatypes`  (
-  `pk` int(10) UNSIGNED NOT NULL AUTO_INCREMENT
-  ,`bitField` bit(1) NOT NULL DEFAULT b'0'
-  ,`dateField` date NOT NULL
-  ,`datetimeField` datetime NOT NULL
-  ,`textField` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL 
-  ,`tinyintField` tinyint(1) UNSIGNED NOT NULL
-  ,`varcharField` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
+  `pk` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT
+  ,`bitField` BIT(1) NULL
+  ,`booleanField` BOOLEAN NULL
+  ,`dateField` DATE NULL
+  ,`datetimeField` DATETIME NULL
+  ,`intField` INT(10) NULL
+  ,`textField` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL 
+  ,`tinyintField` TINYINT(1) UNSIGNED NULL
+  ,`varcharField` VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL
   ,PRIMARY KEY (`pk`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
 SET FOREIGN_KEY_CHECKS = 1;
