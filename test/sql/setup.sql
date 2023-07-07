8e2b51a4fe3cc58b147714f497cffdf464a7d6cc
cfsimplicity <github@zyde.uk>
2023-07-07 15:45:16 +0100

    v3.1.4

--- test/sql/setup.sql
+++ test/sql/setup.sql
@@ -8,7 +8,8 @@ CREATE TABLE `datatypes`  (
   ,`datetimeField` DATETIME NULL
   ,`intField` INT(10) NULL
   ,`textField` TEXT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL 
-  ,`tinyintField` TINYINT(1) UNSIGNED NULL
+  ,`tinyintField` TINYINT(3) NULL
+  ,`tinyintOneField` TINYINT(1) UNSIGNED NULL
   ,`varcharField` VARCHAR(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL
   ,PRIMARY KEY (`pk`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
